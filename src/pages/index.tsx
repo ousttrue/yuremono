cbaa2c5955189a6dc058a93e8459cca4513db0d6
ousttrue <ousttrue@gmail.com>
2024-08-02 08:28:21 +0900

    wasm build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import { Redirect } from '@docusaurus/router';
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 
-function Home() {
-  return <Redirect to={useBaseUrl('docs')} />;
-}
-
-export default Home;
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <main>
+        <div className="container">
+          <Link
+            className="button button--secondary button--lg"
+            target="_blank"
+            to="/wasm/yuremono.html">
+            zig wasm ⚡
+          </Link>
+        </div>
+      </main>
+    </Layout>
+  );
+};
